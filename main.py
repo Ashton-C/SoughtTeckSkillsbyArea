b5f10f0ca03b8fddfc0ba074d5b5da7b574a8aae
ashton_c <armadillomaster@gmail.com>
2018-09-05 20:40:21 -0600

    word bank and beginning of functionality
    Added the word bank of the top 50 most popular programming languages from the TIOBE index and some of the more popular JS and python libraries, as well as Databases and tools and such. Also added the first part of functionality, so it can be run and will report back the amount of hits for each word in the wordbank.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
+from languages import *
 
 
 def main():
@@ -6,6 +8,7 @@ def main():
     desired_position = find_title()
     location = find_location()
     print("Okay, you are looking for a {} job in {}, right? Calculating skills!".format(desired_position, location))
+    get_indeed_listings(desired_position, location)
 
 
 def intro():
@@ -31,8 +34,56 @@ def find_location():
     return user_loc
 
 
+def print_results(field, text):
+    for word in field:
+        if word in text:
+            field[word] += 1
+
+
+def remove_empty(field):
+    to_remove = []
+    for word in field:
+        if field[word] == 0:
+            to_remove.append(word)
+    for word in to_remove:
+        field.pop(word)
+
+
 def get_indeed_listings(position, loc):
-    pass
+    loc = loc.replace(" ", "%20")
+    position = position.replace(" ", "+")
+    counter = 0
+    lpn = 0
+    for i in range(0, 5):
+        listings_page = "https://www.indeed.com/jobs?q={}&l={}&start={}".format(position, loc, lpn)
+        print(listings_page)
+        response = requests.get(listings_page)
+        lps = BeautifulSoup(response.content, 'html.parser')
+        box = lps.find_all(attrs={"data-tn-component": "organicJob"})
+        for result in box:
+            print(result.children)
+            y = result.h2.a['href']
+            listing_page = y
+            new_req = requests.get("https://www.indeed.com{}".format(listing_page))
+            new_soup = BeautifulSoup(new_req.content, 'html.parser')
+            try:
+                description = new_soup.find(attrs={'class': 'jobsearch-JobComponent-description'}).get_text()
+            except AttributeError:
+                description = new_soup.find(attrs={'class': 'jobsearch-JobComponent'})
+                if description is not None:
+                    description = description.get_text()
+                continue
+            print_results(all_languages, description)
+            print_results(all_frameworks, description)
+            print_results(all_databases, description)
+            print_results(all_platforms, description)
+            counter += 1
+        lpn += 10
+    remove_empty(all_languages)
+    remove_empty(all_frameworks)
+    remove_empty(all_databases)
+    remove_empty(all_platforms)
+    print(all_languages, all_frameworks, all_databases, all_platforms)
 
 
 def get_dice_listings(position, loc):
